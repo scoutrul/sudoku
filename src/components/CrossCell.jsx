f5900d3cf8f0200247ac38b948a2837d1e1e40a7
Anton <antongolova@gmail.com>
2021-12-28 02:22:13 +0300

    some logic

--- src/components/CrossCell.jsx
+++ src/components/CrossCell.jsx
@@ -1,26 +1,32 @@
 import styled from 'styled-components';
 
-const CellCSS = styled.div`
-  display: flex;
-  background-color: #e9e9e9;
-  width: 40px;
-  height: 40px;
-  border: 3px double #3d3939;
-  box-sizing: border-box;
-  justify-content: center;
-  align-content: center;
-`;
+const Component = ({item, x, y, toggleCell}) => {
+    const CellCSS = styled.div`
+      display: flex;
+      background-color: ${item ? '#FFF' : '#e9e9e9'};
+      width: 40px;
+      height: 40px;
+      border: 3px double #3d3939;
+      box-sizing: border-box;
+      justify-content: center;
+      align-content: center;
+      user-select: none;
+    `;
+    const NumberCSS = styled.span`
+      font-size: 12px;
+      line-height: 30px;
+      font-weight: 800;
+      color: #4a394b;
+      cursor: pointer;
+      display: flex;
+      width: 100%;
+      height: 100%;
+      align-items: center;
+      justify-content: center;
+    `;
 
-const NumberCSS = styled.span`
-  font-size: 12px;
-  line-height: 30px;
-  font-weight: 800;
-  color: #4a394b;
-`;
-
-const Component = ({item, x, y}) => {
     return <CellCSS>
-        <NumberCSS>x{x} y{y}</NumberCSS>
+        <NumberCSS onClick={item ? null : toggleCell}>{item ? item : ''}</NumberCSS>
     </CellCSS>
 }
 
--- src/components/CrossTable.jsx
+++ src/components/CrossTable.jsx
@@ -1,24 +1,48 @@
+import {useState} from 'react'
 import styled from 'styled-components';
 import CrossCell from './CrossCell'
-
 
 const Component = () => {
     let cols = '1fr 1fr 1fr'
     let rows = '1fr 1fr 1fr'
+    let playerCross = 'X'
+    let playerZeros = '0'
+    let currentPlayer = playerCross
+
+    const [cellState, setCell] = useState({});
 
     let printTable = (xCols, yRows) => {
         let x = ''
         let y = ''
         for (let step = 0; step < xCols; step++) {
-            x = x + ' 1fr'
+            x = x + '1fr '
+            // setCell(state => {
+            //     return {...state, [playerCross + step]: currentPlayer}
+            // })
         }
         for (let step = 0; step < yRows; step++) {
-            y = y + ' 1fr'
+            y = y + '1fr '
         }
         cols = x
         rows = y
     }
-    printTable(9, 9)
+    let makeMove = () => {
+        let oppPlayer = currentPlayer === playerCross ? currentPlayer : playerCross
+        //find cell
+    }
+
+    let toggleCell = (y, x, player) => {
+        const key = `y${y}${playerCross}${x}`
+        setCell(state => {
+            return {...state, [key]: player}
+        })
+
+        makeMove()
+
+    }
+
+
+    printTable(3, 3)
 
     const TableCSS = styled.div`
       width: fit-content;
@@ -30,7 +54,9 @@ const Component = () => {
     return <TableCSS>
         {rows.split('1fr').filter(e => e).map((row, rowIndex) => {
             return cols.split('1fr').filter(e => e).map((col, colIndex) => {
-                return <CrossCell item={colIndex} y={colIndex} x={rowIndex}/>
+                return <CrossCell item={cellState[`y${colIndex}x${rowIndex}`]}
+                                  toggleCell={() => toggleCell(colIndex, rowIndex, currentPlayer)}
+                                  key={`y${colIndex}x${rowIndex}`}/>
             })
         })}
 
